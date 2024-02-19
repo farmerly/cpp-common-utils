c98984d011d5ef4e5c88d3146a4daeadb667de49
shaojx <shaojiaxing@wanji.net.cn>
2024-02-19 09:54:59 +0800

    feat: update xmldocument

--- include/xmlutils/TinyXmlDocument.h
+++ include/xmlutils/TinyXmlDocument.h
@@ -7,17 +7,27 @@ namespace xmlutils {
 class TinyXmlDocument
 {
 public:
-    std::string getXmlString();                // 获取xml消息体
-    bool        setXmlString(std::string xml); // 写入xml消息体
-    bool        loadXmlFile(std::string path); // 从文件读取xml消息
-    bool        saveXmlFile(std::string path); // 保存xml消息到文件
-    void        addDeclaration();              // 添加声明
+    TinyXmlDocument();
+    TinyXmlDocument(const std::string &path);
+    virtual ~TinyXmlDocument();
+
+public:
+    std::string toXmlString();                          // 获取xml消息体
+    bool        parseXmlString(const std::string &xml); // 写入xml消息体
+    bool        loadXmlFile(const std::string &path);   // 从文件读取xml消息
+    bool        saveXmlFile(const std::string &path);   // 保存xml消息到文件
+    void        addDeclaration();                       // 添加声明
+    bool        parseSuccess();                         // 获取是否解析成功
+
+private:
+    bool setParseResult(bool result); // 设置解析结果
 
 public:
     std::shared_ptr<TinyXmlElement> addRootElement(std::string name); // 添加根节点
     std::shared_ptr<TinyXmlElement> getRootElement();                 // 获取根节点
 
 private:
-    tinyxml2::XMLDocument m_doc;
+    tinyxml2::XMLDocument m_xmlDocument;
+    bool                  m_loadSuccess;
 };
 } // namespace xmlutils
--- src/xmlutils/TinyXmlDocument.cpp
+++ src/xmlutils/TinyXmlDocument.cpp
@@ -4,38 +4,66 @@
 
 using namespace xmlutils;
 
-std::string TinyXmlDocument::getXmlString()
+TinyXmlDocument::TinyXmlDocument()
+{
+    m_loadSuccess = false;
+}
+
+TinyXmlDocument::TinyXmlDocument(const std::string &path)
+{
+    auto retCode = m_xmlDocument.LoadFile(path.c_str());
+    setParseResult(retCode == tinyxml2::XML_SUCCESS);
+}
+
+TinyXmlDocument::~TinyXmlDocument()
+{
+}
+
+std::string TinyXmlDocument::toXmlString()
 {
     tinyxml2::XMLPrinter printer;
-    m_doc.Print(&printer);
+    m_xmlDocument.Print(&printer);
     return printer.CStr();
 }
 
-bool TinyXmlDocument::setXmlString(std::string xml)
+bool TinyXmlDocument::parseXmlString(const std::string &xml)
 {
-    return tinyxml2::XML_SUCCESS == m_doc.Parse(xml.c_str());
+    auto retCode = m_xmlDocument.Parse(xml.c_str());
+    return setParseResult(retCode == tinyxml2::XML_SUCCESS);
 }
 
-bool TinyXmlDocument::loadXmlFile(std::string path)
+bool TinyXmlDocument::loadXmlFile(const std::string &path)
 {
-    return tinyxml2::XML_SUCCESS == m_doc.LoadFile(path.c_str());
+    auto retCode = m_xmlDocument.LoadFile(path.c_str());
+    return setParseResult(retCode == tinyxml2::XML_SUCCESS);
 }
 
-bool TinyXmlDocument::saveXmlFile(std::string path)
+bool TinyXmlDocument::saveXmlFile(const std::string &path)
 {
-    return tinyxml2::XML_SUCCESS == m_doc.SaveFile(path.c_str());
+    auto retCode = m_xmlDocument.SaveFile(path.c_str());
+    return retCode == tinyxml2::XML_SUCCESS;
 }
 
 void TinyXmlDocument::addDeclaration()
 {
-    tinyxml2::XMLDeclaration *declaration = m_doc.NewDeclaration();
-    m_doc.InsertFirstChild(declaration);
+    auto declaration = m_xmlDocument.NewDeclaration();
+    m_xmlDocument.InsertFirstChild(declaration);
+}
+
+bool TinyXmlDocument::parseSuccess()
+{
+    return m_loadSuccess;
+}
+
+bool TinyXmlDocument::setParseResult(bool result)
+{
+    return m_loadSuccess = result;
 }
 
 std::shared_ptr<TinyXmlElement> TinyXmlDocument::addRootElement(std::string name)
 {
-    tinyxml2::XMLElement *root = m_doc.NewElement(name.c_str());
-    if (!m_doc.InsertEndChild(root)) {
+    auto root = m_xmlDocument.NewElement(name.c_str());
+    if (!m_xmlDocument.InsertEndChild(root)) {
         return nullptr;
     }
     return std::make_shared<TinyXmlElement>(root);
@@ -44,7 +72,7 @@ std::shared_ptr<TinyXmlElement> TinyXmlDocument::addRootElement(std::string name
 std::shared_ptr<TinyXmlElement> TinyXmlDocument::getRootElement()
 {
     tinyxml2::XMLElement *element = nullptr;
-    if (!(element = m_doc.RootElement())) {
+    if (!(element = m_xmlDocument.RootElement())) {
         return nullptr;
     }
     return std::make_shared<TinyXmlElement>(element);
